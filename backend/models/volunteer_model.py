cbdda1a94d9db899c892374258ab3b5d74cfba2c
Roman Cudreavtev <cudreavtev.roman@gmail.com>
2020-09-12 18:22:04 +0300

    Create Request model

--- backend/models/volunteer_model.py
+++ backend/models/volunteer_model.py
@@ -1,6 +1,8 @@
 import calendar
 import json
 from datetime import datetime as dt
+from enum import Enum
+
 from mongoengine import (
     Document,
     StringField,
@@ -195,3 +197,24 @@ class Beneficiary(User):
 
 class Beneficiary_request(User):
     have_money = BooleanField(default=True)
+
+
+class RequestStatus(Document,Enum):
+    new = [1]
+    confirmed = 2,
+    in_process = 3,
+    canceled = 4,
+    solved = 5,
+    archived =6
+
+
+class Request:
+    beneficiary = ReferenceField(Beneficiary,required = True)
+    user = ReferenceField(User,required = True)
+    status = ReferenceField(RequestStatus,required = True)
+    secret = StringField (max_length=100)
+    urgent = BooleanField (default=False)
+    comments = StringField (max_length=1000)
+    has_symptoms = BooleanField (default=False)
+    cluster = ReferenceField(Cluster)
+    created_at = DateTimeField(default=dt.now)
