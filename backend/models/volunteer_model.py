0802bf4772919e2a8275876368f367dccb39c3bc
Roman Cudreavtev <cudreavtev.roman@gmail.com>
2020-09-12 20:13:15 +0300

    User model

--- backend/models/volunteer_model.py
+++ backend/models/volunteer_model.py
@@ -47,6 +47,12 @@ class Tags(Document):
 class User(Document):
     # TODO: Each volunteer must have a reference to operator that was created by
     # created_by = ReferenceField("operator")
+    role = ListField(default=[])
+    availability_hours_start = IntField(min_value=10000000, max_value=99999999)
+    availability_hours_end = IntField(min_value=10000000, max_value=99999999)
+    availability_days = ListField(default=[])
+    address = StringField(required=True, min_length=4)
+    zone = ReferenceField(Zone,required=True)
     first_name = StringField(max_length=50, default="No_First")
     last_name = StringField(max_length=50, default="No_Last")
     email = EmailField(required=True)
@@ -56,7 +62,6 @@ class User(Document):
     last_access = DateTimeField(default=dt.now)
     # we don't delete users just deactivating them
     logins = ListField(default=[])
-
     is_active = BooleanField(default=True)
 
     meta = {"allow_inheritance": True}
@@ -284,6 +289,19 @@ class Zone(Enum):
     telecentru = 6,
     suburbii = 7
 
+class Role(Enum):
+    administrator = 0,
+    coordinator = 1,
+    operator = 2
+
+class WeekDay(Enum):
+    monday = 0,
+    tuesday = 1,
+    wednesday = 2,
+    thursday = 3,
+    friday = 4,
+    saturday = 5,
+    sunday = 6
 
 
 
