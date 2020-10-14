c7830a140191b10b47d925b0c4db2e9bf0dc3b60
Alexandr Ciornii <c_aciornii@zipwhip.com>
2020-10-14 10:28:51 +0300

    Replace json.loads with clean_data

--- backend/models/volunteer_model.py
+++ backend/models/volunteer_model.py
@@ -68,3 +68,8 @@ class Volunteer(Document):
     sent_photo = BooleanField(default=False)
     aggreed_terms = BooleanField(default=False)
     april1 = BooleanField(default=False)
+
+    def clean_data(self) -> dict:
+        data = self.to_mongo()
+        data["_id"] = str(data["_id"])
+        return data
