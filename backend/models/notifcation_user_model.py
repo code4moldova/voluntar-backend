7ca0fe9f0903ea9031562b79e274b95fbf73b74a
serghei-rubailo <serghei.rubailo@gmail.com>
2020-10-18 19:52:57 +0300

    Add notification field according to Class Diagram Change status field to use NotificationStatus enum class

--- backend/models/notifcation_user_model.py
+++ backend/models/notifcation_user_model.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from mongoengine import Document, ReferenceField, DateTimeField
+from mongoengine import Document, ReferenceField, DateTimeField, StringField
 
 from models.notification_model import Notification
 from models.user_model import User
+from models.enum import NotificationStatus
 
 
 class NotificationUser(Document):
     user = ReferenceField(User, required=True)
-    status = ReferenceField(Notification, required=True)
+    status = StringField(choices=[status.name for status in NotificationStatus], required=True)
+    notification = ReferenceField(Notification, required=True)
     created_at = DateTimeField(default=datetime.now())
