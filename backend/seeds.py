e048c8b58251b12e14be30f85b4c465c328bfb47
serghei-rubailo <serghei.rubailo@gmail.com>
2020-10-19 20:59:21 +0300

    Add assignment notifications to users in seeds.py

--- backend/seeds.py
+++ backend/seeds.py
@@ -4,7 +4,7 @@ import click
 from flask.cli import with_appcontext
 from faker import Faker
 
-from endpoints import registerOperator, register_volunteer, registerBeneficiary, register_notification, assign_notification_to_users
+from endpoints import registerOperator, register_volunteer, registerBeneficiary, register_notification
 from models import Beneficiary, Cluster, Request, User, Volunteer, Notification, NotificationUser
 from models.enums import Zone, VolunteerRole, NotificationType, NotificationStatus
 from tests.factories import BeneficiaryFactory, ClusterFactory, RequestFactory, NotificationFactory
@@ -261,15 +261,16 @@ def seed_db_command():
             request=request, type="new_request", subject=fake.sentence(), created_at=fake.date_this_month(),
         )
         notification.save()
-        # Assign new request to user who created Request
-        assign_notification_to_users(notification=notification, users_list=[request.user], status="new")
+        # Assign new notification to user who created Request
+        NotificationUser.assign_notification_to_users(self=notification, status="new")
 
         # Cancelled request
         notification = NotificationFactory(
             request=request, type="canceled_request", subject=fake.sentence(), created_at=fake.date_this_month(),
         )
         notification.save()
-        assign_notification_to_users(notification=notification, users_list=[request.user], status="seen")
+        # Assign seen notification to user who created Request
+        NotificationUser.assign_notification_to_users(self=notification, status="seen")
 
     click.echo("Initialized the database.")
 
