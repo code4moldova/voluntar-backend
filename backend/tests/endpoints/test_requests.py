1c5f9b367a724d9619c852c2bcf9fc9397e4adb7
johnsmithm <moshnoi2000@gmail.com>
2020-10-30 23:55:19 +0200

    fixed cluster id

--- backend/tests/endpoints/test_requests.py
+++ backend/tests/endpoints/test_requests.py
@@ -47,7 +47,7 @@ class TestRequests(ApiTestCase):
                         "created_at": "Sun, 02 Feb 2020 00:00:00 GMT",
                         "volunteer": {
                             "_id": str(req2.cluster.volunteer.id),
-                            "cluster_id": str(req2.cluster.volunteer.id),
+                            "cluster_id": str(req2.cluster.id),
                             "first_name": req2.cluster.volunteer.first_name,
                             "last_name": req2.cluster.volunteer.last_name,
                         },
