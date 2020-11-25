1ab1756a25dd0e39782849f52940c129cf74db90
johnsmithm <moshnoi2000@gmail.com>
2020-11-25 18:40:27 +0200

    create new beneficiary and request at the same time

--- backend/endpoints/user_request.py
+++ backend/endpoints/user_request.py
@@ -5,6 +5,19 @@ from models import Request, User, Beneficiary, Cluster, Operator
 
 log = logging.getLogger("user_request")
 
+beneficiary_fields = [
+    "first_name",
+    "last_name",
+    "phone",
+    "landline",
+    "age",
+    "zone",
+    "address",
+    "special_condition",
+    "latitude",
+    "longitude",
+]
+
 
 def create_request(request_json, created_by):
     """Creates and persists a new user request into database.
@@ -13,7 +26,7 @@ def create_request(request_json, created_by):
     ----------
     request_json : dict
         A dictionary representing the user request details.
-        example {
+        example1 {
                   "beneficiary": "123456",
                   "type":"grocery",
                   "status": "new",
@@ -21,6 +34,22 @@ def create_request(request_json, created_by):
                   "urgent": false,
                   "comments": "string",
                   "has_symptoms": false
+                }
+        example2 {
+                  "beneficiary": "123456",
+                  "type":"grocery",
+                  "status": "new",
+                  "secret": "345%443$$",
+                  "urgent": false,
+                  "comments": "string",
+                  "has_symptoms": false,
+                  "first_name": "str",
+                  "last_name": "str",
+                  "phone": "68633333",
+                  "landline": "68338333",
+                  "age": 60,
+                  "zone": "botanica",
+                  "address": "dacia"
                 }
     Returns
     -------
@@ -34,10 +63,17 @@ def create_request(request_json, created_by):
         created_by = user.get().clean_data()["email"]
     try:
         created_by = Operator.objects.get(email=created_by)
-        beneficiary = Beneficiary.objects.get(id=request_json["beneficiary"])
-        if beneficiary is None:
-            return jsonify({"error": "beneficiary not found"}), 400
-        user_request_data = request_json
+        if "beneficiary" in request_json:
+            beneficiary = Beneficiary.objects.get(id=request_json["beneficiary"])
+            if beneficiary is None:
+                return jsonify({"error": "beneficiary not found"}), 400
+        else:
+            new_beneficiary = {k: request_json[k] for k in beneficiary_fields if k in request_json}
+            new_beneficiary["created_by"] = created_by.clean_data()["email"]
+            beneficiary = Beneficiary(**new_beneficiary)
+            beneficiary.save()
+
+        user_request_data = {k: v for k, v in request_json.items() if k not in beneficiary_fields}
         user_request_data["user"] = created_by
         user_request_data["beneficiary"] = beneficiary
         log.debug(request_json)
