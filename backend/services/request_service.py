34995c8bcab31adfd0dbb0e4b66ac081c420f966
johnsmithm <moshnoi2000@gmail.com>
2020-12-26 21:27:27 +0200

    filter by cluster and return just one request per cluster

--- backend/services/request_service.py
+++ backend/services/request_service.py
@@ -10,6 +10,9 @@ def requests_by_filters(filters, page=1, per_page=10):
     offset = (int(page) - 1) * per_page
     records = Request.objects()
 
+    if filters.get("by_cluster"):
+        return requests_by_cluster(filters, page, per_page)
+
     if filters.get("b_id"):
         beneficiary = Beneficiary.objects(id=filters.get("b_id")).get()
         records = records.filter(beneficiary=beneficiary)
@@ -44,6 +47,77 @@ def requests_by_filters(filters, page=1, per_page=10):
     reqs = []
 
     for record in records.order_by("-created_at").skip(offset).limit(per_page):
+        entry = dict(
+            _id=str(record.id),
+            status=record.status,
+            urgent=record.urgent,
+            type=record.type,
+            number=record.number,
+            comments=record.comments,
+            has_symptoms=record.has_symptoms,
+            created_at=record.created_at,
+        )
+        volunteer = record.cluster and record.cluster.volunteer or None
+        if volunteer:
+            entry.update(
+                dict(
+                    volunteer=dict(
+                        _id=str(volunteer.id),
+                        first_name=volunteer.first_name,
+                        last_name=volunteer.last_name,
+                        cluster_id=str(record.cluster.id),
+                    )
+                )
+            )
+        beneficiary = record.beneficiary or None
+        if beneficiary:
+            entry.update(
+                dict(
+                    beneficiary=dict(
+                        _id=str(beneficiary.id),
+                        first_name=beneficiary.first_name,
+                        last_name=beneficiary.last_name,
+                        latitude=beneficiary.latitude,
+                        longitude=beneficiary.longitude,
+                        zone=beneficiary.zone,
+                    )
+                )
+            )
+
+        reqs.append(entry)
+
+    return jsonify({"list": reqs, "count": count})
+
+
+def requests_by_cluster(filters, page=1, per_page=10):
+    per_page = int(per_page)
+    offset = (int(page) - 1) * per_page
+    records = Request.objects()
+
+    if filters.get("query"):
+        query_search_fields = ["first_name", "last_name", "phone"]
+        beneficiaries = search.model_keywords_search(Beneficiary, query_search_fields, filters.get("query").split())
+        records = records.filter(beneficiary__in=beneficiaries)
+
+    if filters.get("zone"):
+        beneficiaries = Beneficiary.objects(zone=filters.get("zone"))
+        records = records.filter(beneficiary__in=beneficiaries)
+
+    if filters.get("created_at"):
+        beneficiaries = Beneficiary.objects(created_at=filters.get("created_at"))
+        records = records.filter(beneficiary__in=beneficiaries)
+
+    clusters = records.order_by("-created_at").distinct(field="cluster")
+    # return jsonify([str(i.id) for i in clusters])
+    count = len(clusters)
+    reqs = []
+
+    for cluster in clusters[offset * per_page : per_page * (offset + 1)]:
+        record = Request.objects().filter(cluster=cluster)
+        if record.filter(status="in_process"):
+            record = record.filter(status="in_process").first()
+        else:
+            record = record.first()
         entry = dict(
             _id=str(record.id),
             status=record.status,
