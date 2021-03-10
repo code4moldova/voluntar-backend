0891f613b583eaf603c358147ceb1abeeffc0d70
johnsmithm <moshnoi2000@gmail.com>
2021-03-10 21:33:10 +0200

    refactor volunteer model and set default for status

--- backend/models/volunteer_model.py
+++ backend/models/volunteer_model.py
@@ -20,39 +20,9 @@ class Volunteer(Document):
     availability_hours_start = IntField(min_value=8, max_value=22)
     availability_hours_end = IntField(min_value=8, max_value=22)
     availability_days = ListField(StringField(choices=[weekday.name for weekday in WeekDay]), default=[])
-    status = StringField(choices=[vs.value for vs in VolunteerStatus])
+    status = StringField(choices=[vs.value for vs in VolunteerStatus], default=VolunteerStatus.active.value)
     created_at = DateTimeField(default=dt.now)
     created_by = StringField(max_length=500)  # After frontend changes should be ReferenceField(User)
-
-    # Will be deleted after frontend changes
-    city = StringField(max_length=500, required=False)
-    address_old = StringField(max_length=500, required=False)
-    is_active = BooleanField(default=False)
-    zone_address = StringField(max_length=500)
-    # Availability per day in hours
-    availability = StringField(
-        max_length=500, required=False
-    )  # id of type of availability(d2h per daay, 4 hour/week etc)
-    availability_day = StringField(max_length=500, required=False)  # when available for the offer_beneficiary_id
-    offer_beneficiary_id = StringField(max_length=500, required=False)  # offer_beneficiary_id if ok
-    offer_list = ListField(default=[])
-    latitude = FloatField(min_value=0, max_value=50)
-    longitude = FloatField(min_value=0, max_value=50)
-    activity_types = ListField(default=[])  # StringField(choise=('Activity0', 'Activity1'), default='Activity0')
-    profession = StringField(max_length=500)
-    comments = StringField(max_length=5000)
-    offer = StringField(max_length=500)
-    timestamp = StringField(max_length=500)
-    suburbia = StringField(max_length=500)
-    last_temperature = FloatField(min_value=30, max_value=41)
-    need_sim_unite = BooleanField(default=False)
-    new_volunteer = BooleanField(default=False)
-    black_list = BooleanField(default=False)
-    received_cards = BooleanField(default=False)
-    received_contract = BooleanField(default=False)
-    sent_photo = BooleanField(default=False)
-    aggreed_terms = BooleanField(default=False)
-    april1 = BooleanField(default=False)
 
     def clean_data(self) -> dict:
         data = self.to_mongo()
